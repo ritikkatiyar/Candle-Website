351c60fe7217ed28bee7151a083e60debc512eee
Ritik katiyar <55633948+ritikkatiyar@users.noreply.github.com>
2025-04-05 17:20:22 +0530

    sitemap install for seo

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://candle-sandy.vercel.app', // your live URL
+    generateRobotsTxt: true, // also creates robots.txt
+  };
+  
