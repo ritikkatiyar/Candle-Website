6cead7486c4024f665c7b998cb4f02cd91148e56
Ritik katiyar <55633948+ritikkatiyar@users.noreply.github.com>
2026-02-03 21:57:10 +0530

    wrapped user

--- src/app/user/page.jsx
+++ src/app/user/page.jsx
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
 import UserProductsPage from "./products/page";
 
 // app/admin/page.tsx
 export default function UserDashboard() {
     return (
       <div>
-        <UserProductsPage/>
+        <Suspense fallback={<div className="min-h-screen bg-[#121212] text-white flex items-center justify-center">Loading products...</div>}>
+          <UserProductsPage />
+        </Suspense>
       </div>
     );
   }
